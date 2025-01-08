68223cd4b51652c5440a9da2e541f6baf8d31a7b
Fad <fadhilerdyaqasmal@gmail.com>
2025-01-08 23:09:29 +0700

    try create component

---
+++ src/components/Dashboard/Dashboard.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <h1>Dashboard</h1>
+      <p>Selamat datang di Dashboard sederhana!</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "Dashboard",
+  };
+  </script>
+  
+  <style scoped>
+  h1 {
+    color: #4caf50;
+  }
+  </style>
+  
